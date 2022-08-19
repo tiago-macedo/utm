421ed9df58bd307dc608d8d8c43e4f26a88db507
tiago-macedo <tiagomacedo@ufrj.br>
2022-08-19 01:04:38 -0300

    More rubocop stuff

--- lib/yatm/machine/machine.rb
+++ lib/yatm/machine/machine.rb
@@ -1,75 +1,80 @@
 # frozen_string_literal: true
 
 class YATM::Machine
-	attr_reader :tape, :state_machine, :history
-	
-	def initialize(position: 0, content: [nil])
-		@tape = YATM::Tape.new(content, position: position)
-		@state_machine = YATM::StateMachine.new
-		@history = []
-	end
-	
-	def initial_state(...); @state_machine.initial_state(...); end
-	def final_state(...); @state_machine.final_state(...); end
-	def states(...); @state_machine.states(...); end
-	def events(...); @state_machine.events(...); end
-	def event(...); @state_machine.event(...); end
-	
-	def reset(...)
-		@history = []
-		@tape.reset(...)
-		@state_machine.reset
-	end
-	
-	def to_h
-		{
-			state: @state_machine.current_state,
-			final: @state_machine.final_states.include?(@state_machine.current_state),
-			position: @tape.pos
-		}
-	end
-	
-	def to_s; to_h.to_s; end
-	
-	def to_txt
-		<<~TO_S
-		,_______________
-		| State Machine
-		`---------------
-		#{@state_machine}
-		,______
-		| Tape
-		`------
-		#{@tape}
-		TO_S
-	end
-	
-	def step!(n = 1)
-		return if n < 1
-		
-		(1..n).each do
-			result = @state_machine.process!(@tape.read)
-			@history << result
-			break if result[:final]
-			@tape.write result[:write]
-			@tape.move result[:move]
-		end
-		
-		@history.last
-	end
-	
-	def step(...)
-		step!(...) rescue YATM::Error
-	end
-	
-	def run!(max = Float::INFINITY)
-		(1..max).each do
-			latest = step!
-			break to_h if latest[:final]
-		end
-	end
-	
-	def run(...)
-		run!(...) rescue YATM::Error
-	end
+  attr_reader :tape, :state_machine, :history
+
+  def initialize(position: 0, content: [nil])
+    @tape = YATM::Tape.new(content, position: position)
+    @state_machine = YATM::StateMachine.new
+    @history = []
+  end
+
+  def initial_state(...); @state_machine.initial_state(...); end
+
+  def final_state(...); @state_machine.final_state(...); end
+
+  def states(...); @state_machine.states(...); end
+
+  def events(...); @state_machine.events(...); end
+
+  def event(...); @state_machine.event(...); end
+
+  def reset(...)
+    @history = []
+    @tape.reset(...)
+    @state_machine.reset
+  end
+
+  def to_h
+    {
+      state: @state_machine.current_state,
+      final: @state_machine.final_states.include?(@state_machine.current_state),
+      position: @tape.pos
+    }
+  end
+
+  def to_s; to_h.to_s; end
+
+  def to_txt
+    <<~TO_S
+      ,_______________
+      | State Machine
+      `---------------
+      #{@state_machine}
+      ,______
+      | Tape
+      `------
+      #{@tape}
+    TO_S
+  end
+
+  def step!(n = 1)
+    return if n < 1
+
+    (1..n).each do
+      result = @state_machine.process!(@tape.read)
+      @history << result
+      break if result[:final]
+
+      @tape.write result[:write]
+      @tape.move result[:move]
+    end
+
+    @history.last
+  end
+
+  def step(...)
+    step!(...) rescue YATM::Error
+  end
+
+  def run!(max = Float::INFINITY)
+    (1..max).each do
+      latest = step!
+      break to_h if latest[:final]
+    end
+  end
+
+  def run(...)
+    run!(...) rescue YATM::Error
+  end
 end
--- lib/yatm/tape/tape.rb
+++ lib/yatm/tape/tape.rb
@@ -4,88 +4,89 @@ require_relative "tape_errors"
 
 class YATM::Tape
   attr_accessor :pos
-	
-	def initialize(...)
-		reset(...)
-	end
-	
-	def reset(content = [nil], position: 0)
-		@pos, @p_tape = position, content
-		@n_tape = [nil]
-	end
-	
-	def read
-		tape[table_pos]
-	end
-	
-	def write(value)
-		tape[table_pos] = value
-	end
-	
-	def move(direction = YATM::NONE)
-		case direction
-		when YATM::RIGHT
-			@pos += 1
-			expand if table_pos == tape.size
-		when YATM::LEFT
-			@pos -= 1
-			expand if table_pos > tape.size
-		when YATM::NONE
-		else
-			raise InvalidMove
-		end
-	end
-	
-	def to_s
-		full = @n_tape.reverse + @p_tape
-		full.map.with_index do |val, idx|
-			cell_txt(val, idx)
-		end.join(" ")
-			.gsub(/^\[_\] +/, "")
-			.gsub(/(\[_\] )*\[_\]\z/, "")
-			.gsub(/ \z/, "")
-	end
-	
-	def to_txt
-		full = @n_tape.reverse + @p_tape
-		str = ""
-		full.each_with_index do |val, idx|
-			str += "#{idx - @n_tape.size}".rjust(4) + " | #{val.inspect}".ljust(8)
-			str += " <=" if idx == abs_pos
-			str += "\n"
-		end
-		
-		str
-	end
-	
-	private
-	
-	def cell_txt(val, idx)
-		str = (idx == @n_tape.size) ? "|:| " : ""
-		str += (idx == abs_pos) ? ">|" : "["
-		str += (val.nil?) ? "_" : val.to_s
-		str += (idx == abs_pos) ? "|<" : "]"
-		str
-	end
-	
-	def tape
-		@pos >= 0 ?
-			@p_tape :
-			@n_tape
-	end
-	
-	def abs_pos
-		@pos + @n_tape.size
-	end
-	
-	def table_pos
-		@pos >= 0 ?
-			@pos :
-			-@pos - 1
-	end
-	
-	def expand
-		current_size = tape.size
-		(1..current_size).each { tape << nil }
-	end
+
+  def initialize(...)
+    reset(...)
+  end
+
+  def reset(content = [nil], position: 0)
+    @pos = position
+    @p_tape = content
+    @n_tape = [nil]
+  end
+
+  def read
+    tape[table_pos]
+  end
+
+  def write(value)
+    tape[table_pos] = value
+  end
+
+  def move(direction = YATM::NONE)
+    case direction
+    when YATM::RIGHT
+      @pos += 1
+      expand if table_pos == tape.size
+    when YATM::LEFT
+      @pos -= 1
+      expand if table_pos > tape.size
+    when YATM::NONE
+    else
+      raise InvalidMove
+    end
+  end
+
+  def to_s
+    full = @n_tape.reverse + @p_tape
+    full.map.with_index do |val, idx|
+      cell_txt(val, idx)
+    end.join(" ")
+        .gsub(/^\[_\] +/, "")
+        .gsub(/(\[_\] )*\[_\]\z/, "")
+        .gsub(/ \z/, "")
+  end
+
+  def to_txt
+    full = @n_tape.reverse + @p_tape
+    str = ""
+    full.each_with_index do |val, idx|
+      str += (idx - @n_tape.size).to_s.rjust(4) + " | #{val.inspect}".ljust(8)
+      str += " <=" if idx == abs_pos
+      str += "\n"
+    end
+
+    str
+  end
+
+  private
+
+  def cell_txt(val, idx)
+    str = idx == @n_tape.size ? "|:| " : ""
+    str += idx == abs_pos ? ">|" : "["
+    str += val.nil? ? "_" : val.to_s
+    str += idx == abs_pos ? "|<" : "]"
+    str
+  end
+
+  def tape
+    @pos >= 0 ?
+      @p_tape :
+      @n_tape
+  end
+
+  def abs_pos
+    @pos + @n_tape.size
+  end
+
+  def table_pos
+    @pos >= 0 ?
+      @pos :
+      -@pos - 1
+  end
+
+  def expand
+    current_size = tape.size
+    (1..current_size).each { tape << nil }
+  end
 end
